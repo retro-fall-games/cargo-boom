d960dac91ff37dde7c67e16d442fcf082de9525a
Andrew Bliss <andrewgbliss@gmail.com>
2022-01-24 15:34:04 +0900

    updates

--- Assets/Game/Items/Consumables/CargoConsumable.cs
+++ Assets/Game/Items/Consumables/CargoConsumable.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using RFG.Character;
 using RFG.Items;
+using RFG.Weapons;
+
+public enum CargoType { Health, Armor, Ammo, PowerUp };
 
 [CreateAssetMenu(fileName = "New Cargo Consumable", menuName = "RFG/Items/Consumable/Cargo Consumable")]
 public class CargoConsumable : Consumable
 {
-  public int healthToAdd = 1;
-  public int armorToAdd = 1;
-  public int ammoToAdd = 1;
+  public CargoType CargoType;
+  public int Amount;
 
   public override void Consume(Transform transform, Inventory inventory)
   {
     base.Consume(transform, inventory);
-    Debug.Log("Health to add: " + healthToAdd);
+    Debug.Log("Object: " + transform.gameObject.name);
+    switch (CargoType)
+    {
+      case CargoType.Health:
+        HealthBehaviour health = transform.gameObject.GetComponent<HealthBehaviour>();
+        if (health != null)
+        {
+          Debug.Log("Add Health: " + Amount);
+          health.AddHealth(Amount);
+        }
+        break;
+      case CargoType.Armor:
+        HealthBehaviour armor = transform.gameObject.GetComponent<HealthBehaviour>();
+        if (armor != null)
+        {
+          armor.AddArmor(Amount);
+        }
+        break;
+      case CargoType.Ammo:
+        ProjectileWeaponEquipable weapon = inventory.RightHand as ProjectileWeaponEquipable;
+        if (weapon != null)
+        {
+          weapon.AddAmmo(Amount);
+        }
+        break;
+      case CargoType.PowerUp:
+        Debug.Log("Power Up");
+        break;
+    }
   }
 }
